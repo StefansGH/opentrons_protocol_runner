bec3902b34692ac0e16970eaf05301ed05a11c61
Stefan van Rest <stefan@datascienceadvice.com>
2020-11-23 17:57:36 +0100

    refactor

--- syn96.py
+++ syn96.py
@@ -1,73 +1,45 @@
 from opentrons import protocol_api
 from itertools import product
+import sys
 
 metadata = {'apiLevel': '2.7'}
+
+# get protocal parameter values
+#protocol_params = {}
+#for param in ["ctab", "haucl4", "agno3", "aa", "hcl", "seed"]:
+#    assert param in sys.argv, "Please define " + param
+#    protocol_params[param] = [float(i) for i in sys.argv[sys.argv.index(param) + 1].split(",")]
+protocol_params = {"ctab": [200], "haucl4": [2], "agno3": [2,3,4,5], "aa": [1.6], "hcl": [1,3,5,19,15,20], "seed": [0.2,0.5,1.5]}
+protocol_params = {"ctab": [2], "haucl4": [2], "agno3": [2,3], "aa": [1.6], "hcl": [1,3], "seed": [0.2,0.5]}
+
 
 wells = [i[0]+str(i[1]) for i in list(product(list('ABCDEFGH'), range(1,13)))]
 
 def run(protocol: protocol_api.ProtocolContext):
-    wellplate = protocol.load_labware('corning_96_wellplate_360ul_flat', 1)  #Polystyrene 96 well, flat bottom plate, max 323 ul?
-    tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 2) #small
-    tiprack_2 = protocol.load_labware('opentrons_96_tiprack_300ul', 3) #big
-    tuberack_s = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 4)
-    tuberack_l = protocol.load_labware('opentrons_6_tuberack_falcon_50ml_conical', 5) # hebben we dat?
-    resevoir = protocol.load_labware('axygen_1_reservoir_90ml', 6) #?
-    p10 = protocol.load_instrument('p50_single', 'left', tip_racks=[tiprack_1]) #1-10
-    p50 = protocol.load_instrument('p50_single', 'right', tip_racks=[tiprack_2]) #5-50
-    #p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_2]) #30-300
+    tuberack = protocol.load_labware('opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap', 4)
+    wellplate = protocol.load_labware('nest_96_wellplate_200ul_flat', 1)
+    tiprack_10 = protocol.load_labware('opentrons_96_tiprack_10ul', 2)
+    tiprack_300 = protocol.load_labware('opentrons_96_tiprack_300ul', 3)
+    p10 = protocol.load_instrument('p10_single', 'left', tip_racks=[tiprack_10]) #1-10
+    p50 = protocol.load_instrument('p50_single', 'right', tip_racks=[tiprack_300]) #5-50
 
-    # CTAB
-    #p50.pick_up_tip()
-    #for _ in range(4):
-    #    for w in wells:
-    #        p50.aspirate(50, resevoir['A1'])
-    #        p50.dispense(50, wellplate[w])
-    #p50.return_tip()
-
-    #HAuCl4
-    #p10.pick_up_tip()
-    #for w in wells:
-    #    p10.aspirate(2, tuberack_s['A1'])
-    #    p10.dispense(2, wellplate[w])
-    #p10.return_tip()
-
-    #AgNO3
-    #p10.pick_up_tip()
-    #volumes = [2,3,4,5]*8
-    #for i, r in enumerate('ABCDEFGH'):
-    #    volume_i = volumes[i]
-    #    for c in range(1,13):
-    #        w = r+str(c)
-    #        v = volume_i
-    #        while v>10: #if volume exceeds pipette volume
-    #            p10.aspirate(10, tuberack_s['B1'])
-    #            p10.dispense(10, wellplate[w])
-    #            v -= 10
-    #        p10.aspirate(v, tuberack_s['B1'])
-    #        p10.dispense(v, wellplate[w])
-    #p10.return_tip()    
+    param_sets = list(product(*protocol_params.values())) # for every well a combination of volumes eg. (20,3,1.6,15)
     
-    #Ascorbic Acid
-    #p10.pick_up_tip()
-    #for w in wells:
-    #    p10.aspirate(1.6, tuberack_s['C1'])
-    #    p10.dispense(1.6, wellplate[w])
-    #p10.return_tip()
+    for param_idx in range(len(protocol_params)):
+        if min(list(protocol_params.values())[param_idx])<5: #select pipette
+            pipette = p10
+            v_max = 10
+        else:
+            pipette = p50
+            v_max = 50
 
-    #HCl
-    p10.pick_up_tip()
-    volumes = [1,3,5,10,15,20]*12
-    for i, c in enumerate(range(1,13)):
-        volume_i = volumes[i]
-        for r in 'ABCDEFGH':
-            w = r+str(c)
-            v = volume_i
-            while v>10: #if volume exceeds pipette volume
-                p10.aspirate(10, tuberack_s['B1'])
-                p10.dispense(10, wellplate[w])
-                v -= 10
-            p10.aspirate(v, tuberack_s['B1'])
-            p10.dispense(v, wellplate[w])
-    p10.return_tip()     
-
-    #Seed
+        pipette.pick_up_tip()
+        for well_idx in range(min(96, len(param_sets))):
+            v = param_sets[well_idx][param_idx]
+            _v = v
+            while _v>v_max:
+                pipette.aspirate(v_max, tuberack['A1'])
+                pipette.dispense(v_max, wellplate.wells()[well_idx])
+            pipette.aspirate(_v, tuberack['A1'])
+            pipette.dispense(_v, wellplate.wells()[well_idx])
+        pipette.return_tip()
