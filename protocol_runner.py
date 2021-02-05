0c4a4f3a5689107ae3ec7d51ad6b5a9f562aec03
Stefan van Rest <stefan@datascienceadvice.com>
2021-02-05 22:22:58 +0100

    improve mixing

--- protocol_runner.py
+++ protocol_runner.py
@@ -3,21 +3,23 @@ import opentrons.simulate
 import opentrons.execute
 import socket
 import sys
+import random
+import math
 
-host = str(sys.argv[1]) #ip #socket.gethostbyname(socket.gethostname()) 
+host = str(sys.argv[1]) #ip
 port = 65432
 
 opentrons_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 opentrons_socket.bind((host, port))
 
 data, _ = opentrons_socket.recvfrom(1024)
-data = data.decode('utf-8').split(',') #data is a string "material,units,simulate,well,close,return_tips
+data = data.decode('utf-8').split(',') #data is a string "material,units,simulate,well,close,return_tips,starting_tip_10,starting_tip_50,mix
 simulate = data[3]=="True"
 
 if not simulate:
-    protocol = opentrons.execute.get_protocol_api('2.7')
+    protocol = opentrons.execute.get_protocol_api('2.8')
 else:
-    protocol = opentrons.simulate.get_protocol_api('2.7')
+    protocol = opentrons.simulate.get_protocol_api('2.8')
 
 return_tips = data[6]=="True"
 
@@ -53,15 +55,20 @@ tiprack_200 = protocol.load_labware_from_definition(smartprobes_tiprack_200, 5)
 p10 = protocol.load_instrument('p10_single', 'left', tip_racks=[tiprack_10]) #1-10
 p10.well_bottom_clearance.aspirate = -1
 p10.well_bottom_clearance.dispense = -1
+p10.starting_tip = tiprack_10.well(data[7])
 
 p50 = protocol.load_instrument('p50_single', 'right', tip_racks=[tiprack_200]) #5-50
 p50.well_bottom_clearance.aspirate = 5
 p50.well_bottom_clearance.dispense = 5
+p50.starting_tip = tiprack_200.well(data[8])
 
 ### wellplate ###
 with open('src/hardware/smartprobes_96_wellplate_200ul_flat.json') as labware_file:
     smartprobes_wellplate = json.load(labware_file)
 wellplate = protocol.load_labware_from_definition(smartprobes_wellplate, 3)
+
+protocol_lentgh = 0
+volume_in_well = {}
 
 ### protocol ###
 protocol.home()
@@ -69,43 +76,65 @@ while data[5]=='False': #intil close==True
     material = data[0]
     concentration = float(data[1])
     volume = float(data[2])
-    well = wellplate.wells()[int(data[4])]
-   
-    if volume <= 10: 
-        pipette, pipette_max_volume = p10, 10
-        pipette.flow_rate.dispense = 20
-    else:
-        pipette, pipette_max_volume = p50, 50
-        pipette.flow_rate.dispense = 100
-
-    if (material, concentration) not in [(tuberack_materials[w]['material'], tuberack_materials[w]['concentration']) for w in list(tuberack_materials)]: #check for wrongwriting in materials
-        print(str(material) + ' ' + str(concentration) + ' not in tuberack!')
-    tube_label = find_tube_with_enough_volume(material, concentration, volume)
-    tube, tube_volume = tube_label, tuberack_materials[tube_label]['volume']
-
-    remaining_volume_to_pipette = volume
-    while remaining_volume_to_pipette>0:
+    well_label = int(data[4])
+    well = wellplate.wells()[well_label]
+    
+    if data[9]=='True': #mix
+        pipette = p50
+        pipette.flow_rate.dispense = 500
         pipette.pick_up_tip()
-        v = min(pipette_max_volume, remaining_volume_to_pipette)
-        tuberack_materials = update_tuberack_volumes(tuberack_materials, tube_label, volume)
-        remaining_volume_to_pipette -= v
-        pipette.aspirate(v, tuberack[tube].bottom())
-        pipette.dispense(v, well, rate=10.0)
-        pipette.mix(7, pipette_max_volume, well)
-        pipette.blow_out(well)
-        pipette.touch_tip(well, v_offset=-5, radius=1.3)
-        pipette.blow_out(well)
-        volume -= pipette_max_volume
-
+        volume_hight = (volume_in_well[well_label]-50) / (math.pi*((well.diameter/2)**2))
+        for _ in range(10):
+            pipette.aspirate(volume=50, location=well.bottom(z=volume_hight*random.random()))
+            pipette.dispense(volume=50, location=well.bottom(z=volume_hight*random.random()))
         if return_tips:
             pipette.return_tip()
         else:
             pipette.drop_tip() #thrash
 
+    else: #pipette
+
+        if volume <= 10: 
+            pipette, pipette_max_volume = p10, 10
+            pipette.flow_rate.dispense = 20
+            
+        else:
+            pipette, pipette_max_volume = p50, 50
+            pipette.flow_rate.dispense = 100
+
+        if (material, concentration) not in [(tuberack_materials[w]['material'], tuberack_materials[w]['concentration']) for w in list(tuberack_materials)]: #check for wrongwriting in materials
+            print(str(material) + ' ' + str(concentration) + ' not in tuberack!')
+        tube_label = find_tube_with_enough_volume(material, concentration, volume)
+        tube, tube_volume = tube_label, tuberack_materials[tube_label]['volume']
+
+        remaining_volume_to_pipette = volume
+        while remaining_volume_to_pipette>0:
+            pipette.pick_up_tip()
+            v = min(pipette_max_volume, remaining_volume_to_pipette)
+            tuberack_materials = update_tuberack_volumes(tuberack_materials, tube_label, v)
+            remaining_volume_to_pipette -= v
+            pipette.aspirate(v, tuberack[tube].bottom())
+            pipette.dispense(v, well)
+            pipette.blow_out(well)
+            pipette.touch_tip(well, v_offset=-5, radius=1.3)
+            pipette.blow_out(well)
+            volume -= pipette_max_volume
+
+            if well_label not in volume_in_well:
+                volume_in_well[well_label] = v
+            else:
+                volume_in_well[well_label] = volume_in_well[well_label] + v
+
+            if return_tips:
+                pipette.return_tip()
+            else:
+                pipette.drop_tip() #thrash
+
     data, _ = opentrons_socket.recvfrom(1024) #get next action from server
     data = data.decode('utf-8').split(',')
 
+    print("\n".join(protocol._commands[protocol_lentgh:]))
+    protocol_lentgh = len(protocol._commands)
+
 protocol.home()
 opentrons_socket.close()
-
-print("\n".join(protocol._commands))
